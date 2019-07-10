e9f2cac7e967788cd1ea2468e2862463fd3a0cd6
Lukas Innerhofer <lukas.ihofer@gmail.com>
2019-07-10 21:36:45 +0200

    Implemented RLE de- & encoding. Implemented Huffman encoding. May or may not be correct. Probably is.

--- compression.cpp
+++ compression.cpp
@@ -80,9 +80,11 @@ namespace compression
 	{
 		struct node_t
 		{
+			static std::vector<bool> tempBitset;
+
 			char byte = 0;
 			uint64_t occurences = 0;
-			std::pair<std::shared_ptr<node_t>, std::shared_ptr<node_t>> children = { nullptr, nullptr };
+			std::pair<node_t *, node_t *> children = { nullptr, nullptr };
 
 			node_t() {}
 			node_t(const char byte, const uint64_t &occurences)
@@ -90,21 +92,85 @@ namespace compression
 				this->byte = byte;
 				this->occurences = occurences;
 			}
-			node_t(const std::shared_ptr<node_t> child0, const std::shared_ptr<node_t> child1)
+			node_t(node_t *child0, node_t *child1)
 			{
 				children.first = child0;
 				children.second = child1;
 			}
+			node_t(node_t *child0, node_t *child1, const uint64_t &occurences) : node_t(child0, child1)
+			{
+				this->occurences = occurences;
+			}
+
+			void getCode(std::map<char, std::vector<bool>> &nodes)
+			{
+				if (children.first != nullptr && children.second != nullptr)
+				{
+					tempBitset.push_back(true);
+					children.first->getCode(nodes);
+					tempBitset.erase(tempBitset.end() - 1);
+					tempBitset.push_back(false);
+					children.second->getCode(nodes);
+					tempBitset.erase(tempBitset.end() - 1);
+				}
+				else
+				{
+					nodes[byte] = tempBitset;
+				}
+			}
 		};
+
+		std::vector<bool> node_t::tempBitset = {};
+
+		namespace header
+		{
+			/*
+			* \brief		Serialize huffman code into a format which can be written into a file header
+			* \param[out]	code Mapping of byte to huffman code
+			* \return		Byte string representation of \p code
+			*/
+			std::vector<char> generate(const std::map<char, std::vector<bool>> &code)
+			{
+				std::vector<char> header;
+				int16_t itBits = 0, itCodeBits = 0;
+
+				for (std::pair<char, std::vector<bool>> pair : code)
+				{
+					header.push_back(pair.first);
+					header.push_back(pair.second.size());
+					header.push_back(0);
+
+					for (itCodeBits = pair.second.size() - 1, itBits = 7; itCodeBits >= 0; --itCodeBits, --itBits)
+					{
+						header[header.size() - 1] |= (pair.second[itCodeBits] << itBits);
+						
+						if (itBits == 0)
+						{
+							itBits = 7;
+							header.push_back(0);
+						}
+					}
+				}
+
+				header.insert(header.begin(), static_cast<char>(header.size() + 2));
+				header.insert(header.begin(), static_cast<char>((header.size() + 1) >> 8));
+
+				return header;
+			}
+		}
 
 		/**
 		* \brief		Huffman encode entire dataset
 		*/
 		std::vector<char> encode(const std::vector<char> &dataIn)
 		{
+			std::vector<char> dataOut;
+			dataOut.push_back(0);
+
 			std::map<char, uint64_t> byteOccurences = {};
-			std::vector<std::shared_ptr<node_t>> rootNodes;
+			std::vector<node_t *> rootNodes;
 			std::vector<bool> bits;
+			node_t node;
 
 			for (char byte : dataIn)
 			{
@@ -113,23 +179,49 @@ namespace compression
 
 			for (std::pair<char, uint64_t> pair : byteOccurences)
 			{
-				rootNodes.push_back(std::shared_ptr<node_t>(new node_t(pair.first, pair.second)));
+				rootNodes.push_back(new node_t(pair.first, pair.second));
 			}
 
 			while (rootNodes.size() > 1)
 			{
-				std::sort(rootNodes.begin(), rootNodes.end(), [](const std::shared_ptr<node_t> node0, const std::shared_ptr<node_t> node1) { return node0->occurences < node1->occurences; });
+				std::sort(rootNodes.begin(), rootNodes.end(), [](const node_t *node0, const node_t *node1) { return node0->occurences < node1->occurences; });
 
 				if (rootNodes.size() >= 2)
 				{
-					rootNodes.push_back(std::shared_ptr<node_t>(new node_t(rootNodes[0], rootNodes[1])));
+					rootNodes.push_back(new node_t(rootNodes[0], rootNodes[1], rootNodes[0]->occurences + rootNodes[1]->occurences));
 					rootNodes.erase(rootNodes.begin(), rootNodes.begin() + 2);
 				}
 			}
 
+			std::map<char, std::vector<bool>> code;
+			rootNodes[0]->getCode(code);
 
+			dataOut = header::generate(code);
 
-			return std::vector<char>();
+			int8_t itBits = 7;
+			int64_t itCodeBits = 0;
+			for (char byte : dataIn)
+			{
+				itCodeBits = code[byte].size() - 1;
+				while (itCodeBits >= 0)
+				{
+					dataOut[dataOut.size() - 1] |= (code[byte][itCodeBits] << itBits);
+					--itCodeBits;
+					if (--itBits < 0)
+					{
+						itBits = 7;
+						dataOut.push_back(0);
+					}
+				}
+			}
+
+			// cleanup
+			for (node_t *node : rootNodes)
+			{
+				delete node;
+			}
+
+			return dataOut;
 		}
 
 		/**
--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main()
 	std::vector<char> inData = {};
 	char byte;
 
-	ifs = std::ifstream("in", std::ios_base::binary);
+	ifs = std::ifstream(R"(D:\Users\lukas\Documents\visual studio projects\Compression\Release\in)", std::ios_base::binary);
 	ofs = std::ofstream("ot", std::ios_base::binary);
 
 	while (ifs.get(byte))
 	{
 		inData.push_back(static_cast<uint8_t>(byte));
 	}
-	inData = { 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'C', 'C' };
+	//inData = { 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'C', 'C' };
 	for (uint8_t outByte : compression::huffman::encode(inData))
 	{
-		//ofs << static_cast<char>(outByte);
+		ofs << static_cast<char>(outByte);
 	}
 
 	/*ifs = std::ifstream("ot", std::ios_base::binary);
